ca4e21aee64364598138f881851d013a38789b3c
Mintsuu <jaredchua.jr@gmail.com>
2025-04-03 13:34:31 +0800

    initialisation

---
+++ backend/src/test/java/sg/edu/nus/team3/shoppingcart/ShoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.team3.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
