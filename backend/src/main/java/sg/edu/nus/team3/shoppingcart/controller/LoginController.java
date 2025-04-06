ff01e9d45b9f20053c4d1f8987a261b431c2cacd
Montichiari <124030799+Montichiari@users.noreply.github.com>
2025-04-06 20:30:28 +0800

    Edited LoginController

--- backend/src/main/java/sg/edu/nus/team3/shoppingcart/controller/LoginController.java
+++ backend/src/main/java/sg/edu/nus/team3/shoppingcart/controller/LoginController.java
@@ -1,11 +1,12 @@
 package sg.edu.nus.team3.shoppingcart.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpSession;
 import sg.edu.nus.team3.shoppingcart.model.User;
@@ -15,7 +16,8 @@ import sg.edu.nus.team3.shoppingcart.service.UserService;
 @author diony
 */
 
-@Controller
+@RestController
+@RequestMapping("/")
 public class LoginController {
 	
 	@Autowired
