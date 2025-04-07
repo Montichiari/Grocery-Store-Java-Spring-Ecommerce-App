d4a2e82dd41c735347fa7df34bc630e57a23d7eb
Montichiari <124030799+Montichiari@users.noreply.github.com>
2025-04-07 18:49:53 +0800

    LoginTest changes

--- backend/src/test/java/sg/edu/nus/team3/shoppingcart/LoginTests.java
+++ backend/src/test/java/sg/edu/nus/team3/shoppingcart/LoginTests.java
@@ -1,19 +1,21 @@
 package sg.edu.nus.team3.shoppingcart;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import sg.edu.nus.team3.shoppingcart.repository.UserRepository;
+import sg.edu.nus.team3.shoppingcart.model.User;
+import sg.edu.nus.team3.shoppingcart.serviceimpl.UserServiceImpl;
 
-@DataJpaTest
+@SpringBootTest
 public class LoginTests {
 
 	@Autowired
-	private UserRepository userService;
+	private UserServiceImpl userService;
 	
 	@Test
 	public void testSaveUser() {
@@ -21,19 +23,19 @@ public class LoginTests {
 	}
 	
 	@Test
-	public void userFoundByEmailNotNull(String email) {
+	public void ExistingUserFoundByEmailIsNotNull(String email) {
 		assertNotNull(userService.findUserByEmail("john.doe@example.com"));
 	}
 	
-	/*
+	
 	@Test
 	public void correctUserFoundByEmail(String email) {
 		assertEquals(new User() userService.findUserByEmail("john.doe@example.com"));
 	}
-	*/
+	
 	
 	@Test
-	public void testUserNotFound(String email) {
+	public void NonExistingUserFoundByEmailIsNull(String email) {
 		
 		assertNull(userService.findUserByEmail("nowaythisemailexists@gmail.com"));
 		
