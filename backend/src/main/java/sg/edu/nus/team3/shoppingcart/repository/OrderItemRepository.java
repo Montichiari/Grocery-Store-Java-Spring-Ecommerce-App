a3fe3e05839a1f96d19bcfbb458f860851d8ffdf
Mintsuu <jaredchua.jr@gmail.com>
2025-04-08 16:41:29 +0800

    fixes prior to merge main

--- backend/src/main/java/sg/edu/nus/team3/shoppingcart/repository/OrderItemRepository.java
+++ backend/src/main/java/sg/edu/nus/team3/shoppingcart/repository/OrderItemRepository.java
@@ -3,6 +3,6 @@ package sg.edu.nus.team3.shoppingcart.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sg.edu.nus.team3.shoppingcart.model.OrderItem;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<Integer, OrderItem> {
 
 }
