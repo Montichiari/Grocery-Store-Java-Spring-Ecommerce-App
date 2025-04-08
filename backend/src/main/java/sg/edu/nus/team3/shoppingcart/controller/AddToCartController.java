f451166d26b27dcbc10f92caef8854ff73cbd8e6
ThinaDR <thinadr21@gmail.com>
2025-04-08 14:41:22 +0800

    replaced ShoppingCartController with AddToCartController

---
+++ backend/src/main/java/sg/edu/nus/team3/shoppingcart/controller/AddToCartController.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.team3.shoppingcart.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class AddToCartController {
+
+	
+	
+	
+	
+}
