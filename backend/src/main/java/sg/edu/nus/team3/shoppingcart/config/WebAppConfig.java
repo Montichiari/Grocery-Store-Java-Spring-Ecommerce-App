ac30ef13895be6b15f3cdfb76f53206fb56da479
Montichiari <124030799+Montichiari@users.noreply.github.com>
2025-04-10 20:44:54 +0800

    Added RoleInterceptor and changed implementation

--- backend/src/main/java/sg/edu/nus/team3/shoppingcart/config/WebAppConfig.java
+++ backend/src/main/java/sg/edu/nus/team3/shoppingcart/config/WebAppConfig.java
@@ -6,16 +6,21 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import sg.edu.nus.team3.shoppingcart.interceptor.LoginInterceptor;
+import sg.edu.nus.team3.shoppingcart.interceptor.RoleInterceptor;
 
 @Component
 public class WebAppConfig implements WebMvcConfigurer {
 	@Autowired
 	LoginInterceptor loginInterceptor;
 	
+	@Autowired
+	RoleInterceptor roleInterceptor;
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		// Current interceptor implementation redirects to login from cart and admin urls except Login itself, if not logged in.
 		registry.addInterceptor(loginInterceptor).addPathPatterns("/cart/**", "/admin/**").excludePathPatterns("/login");
+		registry.addInterceptor(roleInterceptor).addPathPatterns("/admin/**").excludePathPatterns("/login");
 	}
 	
 	
