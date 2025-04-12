74275ed7a88e5c4ffc62a34fa98c522501b2605b
Montichiari <124030799+Montichiari@users.noreply.github.com>
2025-04-12 14:17:46 +0800

    Added cartId as a session attribute, and tweaked the CartRepository

--- backend/src/main/java/sg/edu/nus/team3/shoppingcart/repository/ShoppingCartRepository.java
+++ backend/src/main/java/sg/edu/nus/team3/shoppingcart/repository/ShoppingCartRepository.java
@@ -1,16 +1,20 @@
 package sg.edu.nus.team3.shoppingcart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import sg.edu.nus.team3.shoppingcart.model.ShoppingCart;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Integer> {
 
 	// find shopping cart by user id
 	@Query("SELECT s FROM ShoppingCart s WHERE s.user.id = :id")
-	ShoppingCart findShoppingCartByUserId(int id);
+	ShoppingCart findShoppingCartByUserId(@Param("userId") int id);
+	
+	@Query("SELECT s FROM ShoppingCart s WHERE s.id = :cartId")
+	ShoppingCart findShoppingCartByCartId(@Param("cartId") int id);
 
 }
