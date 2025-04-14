b3de717e608ec1908ffe0c29bc2e13afb9c025f6
Montichiari <124030799+Montichiari@users.noreply.github.com>
2025-04-14 22:28:41 +0800

    Added an extra feature for Orders History

--- backend/src/main/java/sg/edu/nus/team3/shoppingcart/controller/OrderHistoryController.java
+++ backend/src/main/java/sg/edu/nus/team3/shoppingcart/controller/OrderHistoryController.java
@@ -1,11 +1,13 @@
 package sg.edu.nus.team3.shoppingcart.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.HttpSession;
 import sg.edu.nus.team3.shoppingcart.model.Order;
 import sg.edu.nus.team3.shoppingcart.model.User;
+import sg.edu.nus.team3.shoppingcart.service.OrderService;
 import sg.edu.nus.team3.shoppingcart.service.UserService;
 
 /**
- * @author Ma Li
+ * @author Ma Li, Dion Yao
  */
 
 @RestController
@@ -25,7 +28,11 @@ public class OrderHistoryController {
 	
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private OrderService orderService;
 
+	// Authored by Ma Li
 	@GetMapping("/history")
 	@ResponseBody
 	public ResponseEntity<?> getOrderHistory(HttpSession session) {
@@ -40,5 +47,29 @@ public class OrderHistoryController {
 		List<Order> orders = user.getOrders();
 
 		return new ResponseEntity<>(orders, HttpStatus.OK);
+	}
+	
+	// Authored by Dion Yao
+	@GetMapping("/history/{id}")
+	public ResponseEntity<?> getOrderDetails(@PathVariable int id, HttpSession session) {
+	    int userId = (int) session.getAttribute("id");
+	    String role = (String) session.getAttribute("role");
+
+	    if (userId == 0) {
+	        return new ResponseEntity<>("You must be logged in to view this page", HttpStatus.UNAUTHORIZED);
+	    }
+
+	    Order order = orderService.getOrderById(id);
+
+	    // Booleans to check if staff, or if logged in customer is viewing an order they "own"
+	    boolean isStaff = role != null && role.equalsIgnoreCase("staff");
+	    boolean isOwner = (order.getUser().getId() == userId);
+	    
+	    // Allow only staff or order owner to view order details
+	    if (!isStaff && !isOwner) {
+	        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+	    }
+
+	    return new ResponseEntity<>(order, HttpStatus.OK);
 	}
 }
